c2ed76e91b589dd8d02506e73a4621ec151c001f
chejianchao <820992376@qq.com>
2018-07-09 01:37:57 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -15,7 +15,7 @@ end
 def apply_coupons(cart, coupons)
   # code here
   coupons.each {|coupon|
-    cart[coupon[:item]] -= coupon[:num]
+    cart[coupon[:item]][:count] -= coupon[:num]
     cart[coupon[:item] + "W/COUPON"] = {:price => coupon[:cost], :clearance => true, :count => coupon[:num]}
   }
   cart
