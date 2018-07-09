d6635b94a88994a8ab43842e965a24750700359d
chejianchao <820992376@qq.com>
2018-07-09 01:22:22 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,10 +1,15 @@
 def consolidate_cart(cart)
   # code here
-  
+  res = {}
+  cart.each{|item|
+    res[item.keys[0]] = item.values[0]
+  }
+  res
 end
 
 def apply_coupons(cart, coupons)
   # code here
+  
 end
 
 def apply_clearance(cart)
